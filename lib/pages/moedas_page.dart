ceca92e650950822440c536ab90ec83e7e8c05a9
Pedro Yago Iraiore Carvalho <pedroiraiore@hotmail.com>
2024-08-31 18:54:26 -0400

    alterando o formato monetario por meio do package intl

--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,28 +1,44 @@
 import 'package:crypto_coin/repositories/moeda_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';//pacote para formartar os valor monetário
 
 class MoedasPage extends StatelessWidget {
   const MoedasPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final tabela = MoedaRepository.tabela; //acessando a tabela e atribuindo a var tabela (final = não vai mudar)
+    final tabela = MoedaRepository
+        .tabela; //acessando a tabela e atribuindo a var tabela (final = não vai mudar)
+    NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');//formatando o valor monetário das cryptomoedas
 
     return Scaffold(
-        appBar: AppBar(
-          //backgroundColor: Colors.blue,
-          title: Text('Crypto Moedas'),
-        ),
-        body: ListView.separated(
-            itemBuilder: (BuildContext context, int moeda){
-              return ListTile(
-                leading: Image.asset(tabela[moeda].icone),
-                title: Text(tabela[moeda].nome),
-                trailing: Text(tabela[moeda].preco.toString()),
-              );
-            },
-            padding: EdgeInsets.all(16),
-            separatorBuilder: (_,__) => Divider(),
-            itemCount: tabela.length,),);
+      appBar: AppBar(
+        //backgroundColor: Colors.blue,
+        title: Text('Crypto Moedas'),
+      ),
+      //mostrando os itens da classe moeda repository na tela da classa moedas page
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int moeda) {
+          return ListTile(
+            leading: SizedBox(
+              child: Image.asset(tabela[moeda].icone),
+              width: 40,
+            ),
+            title: Text(
+              tabela[moeda].nome,
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            trailing: Text(real.format(tabela[moeda].preco),),
+          );
+        },
+        padding: EdgeInsets.all(16),
+        separatorBuilder: (_, __) => Divider(),
+        itemCount: tabela
+            .length, //o flutter precisa saber qual o tamanho da lista pra poder rendereizar
+      ),
+    );
   }
 }
