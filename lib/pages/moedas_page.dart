3262217fe11a796d0abf60ac88cb08c48d20f0c8
Pedro Yago Iraiore Carvalho <pedroiraiore@hotmail.com>
2024-08-31 19:30:04 -0400

    aplicando a logica de selecionar as moedas e alterando a page de moedas para stateful

--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,16 +1,28 @@
+import 'package:crypto_coin/moedas/moeda.dart';
 import 'package:crypto_coin/repositories/moeda_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';//pacote para formartar os valor monetário
+import 'package:intl/intl.dart'; //pacote para formartar os valor monetário
 
-class MoedasPage extends StatelessWidget {
-  const MoedasPage({super.key});
+class MoedasPage extends StatefulWidget {
+
+  MoedasPage({super.key});
+
+  @override
+  _MoedasPageState createState() => _MoedasPageState();
+}
+
+class _MoedasPageState extends State<MoedasPage> {
+  final tabela = MoedaRepository
+      .tabela; 
+ //acessando a tabela e atribuindo a var tabela (final = não vai mudar)
+  NumberFormat real = NumberFormat.currency(
+      locale: 'pt_BR',
+      name: 'R\$'); 
+ //formatando o valor monetário das cryptomoedas
+  List<Moeda> selecionadas = [];
 
   @override
   Widget build(BuildContext context) {
-    final tabela = MoedaRepository
-        .tabela; //acessando a tabela e atribuindo a var tabela (final = não vai mudar)
-    NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');//formatando o valor monetário das cryptomoedas
-
     return Scaffold(
       appBar: AppBar(
         //backgroundColor: Colors.blue,
@@ -20,19 +32,35 @@ class MoedasPage extends StatelessWidget {
       body: ListView.separated(
         itemBuilder: (BuildContext context, int moeda) {
           return ListTile(
-            leading: SizedBox(
-              child: Image.asset(tabela[moeda].icone),
-              width: 40,
-            ),
-            title: Text(
-              tabela[moeda].nome,
-              style: TextStyle(
-                fontSize: 17,
-                fontWeight: FontWeight.w500,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                Radius.circular(12),
+              )),
+              leading: SizedBox(
+                child: Image.asset(tabela[moeda].icone),
+                width: 40,
               ),
-            ),
-            trailing: Text(real.format(tabela[moeda].preco),),
-          );
+              title: Text(
+                tabela[moeda].nome,
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              trailing: Text(
+                real.format(tabela[moeda].preco),
+              ),
+              selected: selecionadas.contains(tabela[moeda]),
+              selectedTileColor: Colors.indigo[50],
+              onLongPress: () {
+                //verificando se já existem itens selecionados e removendo quando pressionados
+                setState(() {
+                  (selecionadas.contains(tabela[moeda]))
+                  ? selecionadas.remove(tabela[moeda])
+                  : selecionadas.add(tabela[moeda]);
+                });
+              },
+            );
         },
         padding: EdgeInsets.all(16),
         separatorBuilder: (_, __) => Divider(),
